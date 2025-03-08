77c69acf60114b572a3a1485a2626988b76ac77b
mukeshbabu-mb <mukeshbabu6671@gmail.com>
2025-03-08 12:49:30 +0530

    Create 05_Remove_List_items.py

---
+++ Python Learning/09_Python_List/05_Remove_List_items.py
@@ -0,0 +1,6 @@
+# This is a Super Heros Name List
+superHeros = ["Captain America", "Iron Man", "Thor", "Hulk", "Black Widow", "Spider Man" ]
+
+print("Before removing \"Thor\" in List : ",superHeros)
+superHeros.remove("Thor")
+print("\nAfter removing \"Thor\" in List : ",superHeros)
