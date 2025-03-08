728942488a95b9af3d09680d9fdf53354b7b90b2
mukeshbabu-mb <mukeshbabu6671@gmail.com>
2025-03-08 13:07:18 +0530

    Create 01_Arithmetic_Operators.py

---
+++ Python Learning/08_Python_Operators/01_Arithmetic_Operators.py
@@ -0,0 +1,14 @@
+# Addition Operator( + )
+print("Addition Operator.       Symbol \" + \".     Example: a + b ")
+
+
+# Subtraction Operator( - )
+print("\nSubtraction Operator.    Symbol \" - \".     Example: a - b ")
+
+
+# Multiplication Operator( * )
+print("\nMultiplication Operator. Symbol \" * \".     Example: a * b ")
+
+
+# Division Operator( + )
+print("\nDivision Operator.       Symbol \" / \".     Example: a / b ")
